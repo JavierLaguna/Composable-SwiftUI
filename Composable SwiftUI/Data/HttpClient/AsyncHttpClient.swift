1870956a8aeefac266f310cf794f43afed592720
Javier Laguna <javier.laguna@innocv.com>
2023-07-22 10:11:08 +0200

    Create new AsyncHttpClient

---
+++ Composable SwiftUI/Data/HttpClient/AsyncHttpClient.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+class AsyncHttpClient {
+    
+    func get<D: Decodable>(from url: URL) async throws -> D {
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        return try JSONDecoder().decode(D.self, from: data)
+    }
+    
+    func post<D: Decodable, E: Encodable>(_ requestData: E, to url: URL) async throws -> D {
+        let bodyData = try JSONEncoder().encode(requestData)
+        var request = URLRequest(url: url)
+        request.httpMethod = HttpConstants.postMethod
+        request.httpBody = bodyData
+        request.addValue(
+            HttpConstants.Headers.applicationJson,
+            forHTTPHeaderField: HttpConstants.Headers.contentType)
+        
+        let (responseData, _) = try await URLSession.shared.data(for: request)
+        
+        return try JSONDecoder().decode(D.self, from: responseData)
+    }
+}
