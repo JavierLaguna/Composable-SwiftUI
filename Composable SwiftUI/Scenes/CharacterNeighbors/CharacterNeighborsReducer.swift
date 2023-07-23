f055f94227921ebf44437246028dd8c7fcd0307c
Javier Laguna <javier.laguna@innocv.com>
2023-07-23 12:45:05 +0200

    Create CharacterNeighborsReducer using new TCA ReducerProtocol

---
+++ Composable SwiftUI/Scenes/CharacterNeighbors/CharacterNeighborsReducer.swift
@@ -0,0 +1,43 @@
+import ComposableArchitecture
+import Resolver
+
+struct CharacterNeighborsReducer: ReducerProtocol {
+    
+    @Injected private var getLocationInfoInteractor: GetLocationInfoInteractor
+    
+    private let locationId: Int
+    
+    init(locationId: Int) {
+        self.locationId = locationId
+    }
+    
+    struct State: Equatable {
+        var locationDetail: StateLoadable<LocationDetail> = .init()
+    }
+    
+    enum Action: Equatable {
+        case getLocationInfo
+        case onGetLocationInfo(TaskResult<LocationDetail>)
+    }
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+        case .getLocationInfo:
+            state.locationDetail.state = .loading
+            
+            return .run { send in
+                await send(.onGetLocationInfo(TaskResult {
+                    try await getLocationInfoInteractor.execute(locationId: locationId)
+                }))
+            }
+            
+        case .onGetLocationInfo(.success(let locationDetail)):
+            state.locationDetail.state = .populated(data: locationDetail)
+            return .none
+            
+        case .onGetLocationInfo(.failure(let error)):
+            state.locationDetail.state = .error(error)
+            return .none
+        }
+    }
+}
