ed0f507b7cc3a106a9cf9f5e8731c85724c4f461
Javier Laguna <javier.laguna@innocv.com>
2024-09-24 18:02:14 +0200

    Restore and fix CharactersListViewTests

--- Composable SwiftUITests/UI/Scenes/CharactersListViewTests.swift
+++ Composable SwiftUITests/UI/Scenes/CharactersListViewTests.swift
@@ -1,159 +1,164 @@
-// import Testing
-// import SwiftUI
-// import ComposableArchitecture
-//
-// @testable import Composable_SwiftUI
-//
-// @Suite("CharactersListView", .tags(.UI))
-// final class CharactersListViewTests: SceneSnapshotUITest {
-//
-//    override var file: StaticString {
-//        #filePath
-//    }
-//
-//    private var store: StoreOf<CharactersListReducer>!
-//
-//    @Test(
-//        "Loading State",
-//        arguments: SceneSnapshotUITest.Variant.allVariants
-//    )
-//    func loadingState(variant: SceneSnapshotUITest.Variant) {
-//        loadingStateSetUp()
-//
-//        execute(
-//            name: "charactersListView_loadingState",
-//            view: CharactersListView(store: store),
-//            variant: variant
-//        )
-//    }
-//
-//    @Test(
-//        "Loading State With Data",
-//        arguments: SceneSnapshotUITest.Variant.allVariants
-//    )
-//    func loadingStateWithData(variant: SceneSnapshotUITest.Variant) {
-//        loadingStateWithDataSetUp()
-//
-//        execute(
-//            name: "charactersListView_loadingStateWithData",
-//            view: CharactersListView(store: store),
-//            variant: variant
-//        )
-//    }
-//
-//    @Test(
-//        "Populated State",
-//        arguments: SceneSnapshotUITest.Variant.allVariants
-//    )
-//    func populatedState(variant: SceneSnapshotUITest.Variant) {
-//        populatedStateSetUp()
-//
-//        execute(
-//            name: "charactersListView_populatedState",
-//            view: CharactersListView(store: store),
-//            variant: variant
-//        )
-//    }
-//
-//    @Test(
-//        "Error State",
-//        arguments: SceneSnapshotUITest.Variant.allVariants
-//    )
-//    func errorState(variant: SceneSnapshotUITest.Variant) {
-//        errorStateSetUp()
-//
-//        execute(
-//            name: "charactersListView_errorState",
-//            view: CharactersListView(store: store),
-//            variant: variant
-//        )
-//    }
-// }
-//
+import Testing
+import SwiftUI
+import ComposableArchitecture
+
+@testable import Composable_SwiftUI
+
+@Suite("CharactersListView", .tags(.UI, .UIScene))
+final class CharactersListViewTests: SceneSnapshotUITest {
+
+    override var file: StaticString {
+        #filePath
+    }
+
+    private var store: StoreOf<CharactersListReducer>!
+
+    @Test(
+        "Loading State",
+        arguments: SceneSnapshotUITest.Variant.allVariants
+    )
+    func loadingState(variant: SceneSnapshotUITest.Variant) {
+        loadingStateSetUp()
+
+        execute(
+            name: "charactersListView_loadingState",
+            view: view,
+            variant: variant
+        )
+    }
+
+    @Test(
+        "Loading State With Data",
+        arguments: SceneSnapshotUITest.Variant.allVariants
+    )
+    func loadingStateWithData(variant: SceneSnapshotUITest.Variant) {
+        loadingStateWithDataSetUp()
+
+        execute(
+            name: "charactersListView_loadingStateWithData",
+            view: view,
+            variant: variant
+        )
+    }
+
+    @Test(
+        "Populated State",
+        arguments: SceneSnapshotUITest.Variant.allVariants
+    )
+    func populatedState(variant: SceneSnapshotUITest.Variant) {
+        populatedStateSetUp()
+
+        execute(
+            name: "charactersListView_populatedState",
+            view: view,
+            variant: variant
+        )
+    }
+
+    @Test(
+        "Error State",
+        arguments: SceneSnapshotUITest.Variant.allVariants
+    )
+    func errorState(variant: SceneSnapshotUITest.Variant) {
+        errorStateSetUp()
+
+        execute(
+            name: "charactersListView_errorState",
+            view: view,
+            variant: variant
+        )
+    }
+}
+
 // MARK: Private methods
-// private extension CharactersListViewTests {
-//
-//    static private func imageUrl(id: Int) -> String {
-//        "\(TestConfig.characterImageBaseUrl)/\(id).jpeg"
-//    }
-//
-//    private var characters: [Character] {
-//        [
-//            Character(
-//                id: 1,
-//                name: "Rick Sanchez",
-//                status: .alive,
-//                species: "Human",
-//                type: "",
-//                gender: .male,
-//                origin: CharacterLocation(
-//                    id: 1,
-//                    name: "Earth"
-//                ),
-//                location: CharacterLocation(
-//                    id: 1,
-//                    name: "Earth"
-//                ),
-//                image: Self.imageUrl(id: 1),
-//                episodes: []
-//            ),
-//            Character(
-//                id: 2,
-//                name: "Morty",
-//                status: .alive,
-//                species: "Human",
-//                type: "",
-//                gender: .male,
-//                origin: CharacterLocation(
-//                    id: 1,
-//                    name: "Earth"
-//                ),
-//                location: CharacterLocation(
-//                    id: 1,
-//                    name: "Earth"
-//                ),
-//                image: Self.imageUrl(id: 2),
-//                episodes: []
-//            )
-//        ]
-//    }
-//
-//    func loadingStateSetUp() {
-//        let state = CharactersListReducer.State(
-//            characters: .init(state: .loading)
-//        )
-//        configureStore(with: state)
-//    }
-//
-//    func loadingStateWithDataSetUp() {
-//        var state = CharactersListReducer.State(
-//            characters: .init(state: .populated(data: characters))
-//        )
-//        state.characters.state = .loading
-//        configureStore(with: state)
-//    }
-//
-//    func populatedStateSetUp() {
-//        let state = CharactersListReducer.State(
-//            characters: .init(state: .populated(data: characters))
-//        )
-//        configureStore(with: state)
-//    }
-//
-//    func errorStateSetUp() {
-//        let error = InteractorError.generic(message: "")
-//        let state = CharactersListReducer.State(
-//            characters: .init(state: .error(error))
-//        )
-//        configureStore(with: state)
-//    }
-//
-//    func configureStore(with state: CharactersListReducer.State) {
-//        store = StoreOf<CharactersListReducer>(
-//            initialState: state,
-//            reducer: {
-//                // Intentionally empty
-//            }
-//        )
-//    }
-// }
+private extension CharactersListViewTests {
+
+    static func imageUrl(id: Int) -> String {
+        "\(TestConfig.characterImageBaseUrl)/\(id).jpeg"
+    }
+
+    var view: some View {
+        CharactersListView(store: store)
+            .allEnvironmentsInjected
+    }
+
+    var characters: [Character] {
+        [
+            Character(
+                id: 1,
+                name: "Rick Sanchez",
+                status: .alive,
+                species: "Human",
+                type: "",
+                gender: .male,
+                origin: CharacterLocation(
+                    id: 1,
+                    name: "Earth"
+                ),
+                location: CharacterLocation(
+                    id: 1,
+                    name: "Earth"
+                ),
+                image: Self.imageUrl(id: 1),
+                episodes: []
+            ),
+            Character(
+                id: 2,
+                name: "Morty",
+                status: .alive,
+                species: "Human",
+                type: "",
+                gender: .male,
+                origin: CharacterLocation(
+                    id: 1,
+                    name: "Earth"
+                ),
+                location: CharacterLocation(
+                    id: 1,
+                    name: "Earth"
+                ),
+                image: Self.imageUrl(id: 2),
+                episodes: []
+            )
+        ]
+    }
+
+    func loadingStateSetUp() {
+        let state = CharactersListReducer.State(
+            characters: .init(state: .loading)
+        )
+        configureStore(with: state)
+    }
+
+    func loadingStateWithDataSetUp() {
+        var state = CharactersListReducer.State(
+            characters: .init(state: .populated(data: characters))
+        )
+        state.characters.state = .loading
+        configureStore(with: state)
+    }
+
+    func populatedStateSetUp() {
+        let state = CharactersListReducer.State(
+            characters: .init(state: .populated(data: characters))
+        )
+        configureStore(with: state)
+    }
+
+    func errorStateSetUp() {
+        let error = InteractorError.generic(message: "")
+        let state = CharactersListReducer.State(
+            characters: .init(state: .error(error))
+        )
+        configureStore(with: state)
+    }
+
+    func configureStore(with state: CharactersListReducer.State) {
+        store = StoreOf<CharactersListReducer>(
+            initialState: state,
+            reducer: {
+                // Intentionally empty
+            }
+        )
+    }
+}
