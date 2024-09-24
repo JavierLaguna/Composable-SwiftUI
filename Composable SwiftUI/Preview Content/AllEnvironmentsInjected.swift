33b6c93058fbaa1a5dabbf88d9d9d2e7a6064674
Javier Laguna <javier.laguna@innocv.com>
2024-09-24 18:01:08 +0200

    Restore coordinators on AllEnvironmentsInjectedModifier

--- Composable SwiftUI/Preview Content/AllEnvironmentsInjected.swift
+++ Composable SwiftUI/Preview Content/AllEnvironmentsInjected.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import ComposableArchitecture
 
 extension View {
 
@@ -11,12 +12,26 @@ extension View {
 
 private struct AllEnvironmentsInjectedModifier: ViewModifier {
 
+    private let mainStore: StoreOf<MainReducer>
+
     @State private var mainCoordinator = MainCoordinator()
-//    @State private var charactersCoordinator = CharactersCoordinator()
+    @State private var charactersCoordinator: CharactersCoordinator
+
+    init() {
+        let mainStore = StoreOf<MainReducer>(
+            initialState: .init(),
+            reducer: {
+                MainReducer()
+            }
+        )
+
+        self.mainStore = mainStore
+        charactersCoordinator = CharactersCoordinator(mainStore: mainStore)
+    }
 
     func body(content: Content) -> some View {
         content
             .environment(mainCoordinator)
-//            .environment(charactersCoordinator)
+            .environment(charactersCoordinator)
     }
 }
