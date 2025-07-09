f92736e8f4aec695f2d40403ad00a9c71cea3a0b
Javier Laguna <javier.laguna@innocv.com>
2025-07-09 12:13:35 +0200

    WIP Use FoundationModels for GetCharacterDescriptionInteractor
    # Conflicts:
    #	Composable SwiftUI/Business/Interactors/Character/GetCharacterDescriptionInteractor.swift

--- Composable SwiftUI/Business/Interactors/Character/GetCharacterDescriptionInteractor.swift
+++ Composable SwiftUI/Business/Interactors/Character/GetCharacterDescriptionInteractor.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Mockable
+import FoundationModels
 
 @Mockable
 protocol GetCharacterDescriptionInteractor: Sendable {
@@ -15,18 +16,14 @@ struct GetCharacterDescriptionInteractorFactory {
 
 struct GetCharacterDescriptionInteractorDefault: GetCharacterDescriptionInteractor, ManagedErrorInteractor {
 
+    private let session = LanguageModelSession()
+
     func execute(character: Character) async throws -> String {
-        if Bool.random() {
-           """
-              Lorem ipsum dolor sit amet consectetur adipiscing, elit cubilia maecenas inceptos rutrum hac, sed faucibus interdum commodo curabitur. Gravida felis leo ut habitant eget in nam turpis vitae, quam taciti nullam aliquet rhoncus quisque dictumst fusce mattis, vel dui maecenas enim morbi sociosqu himenaeos erat. Fusce penatibus dictum pellentesque odio sagittis lobortis fermentum hendrerit mattis placerat, vel sociis facilisi tortor quisque tempus nisi mus primis, iaculis nisl ac egestas leo pulvinar ante quam vulputate.
-              """
+        let response = try await session.respond(
+            to: "Create a good description for a Rick, the character of the famous rick and morty Tv program"
+        )
 
-        } else {
-           """
-              Lorem ipsum dolor sit amet consectetur adipiscing, elit cubilia maecenas inceptos rutrum hac, sed faucibus interdum commodo curabitur. Gravida felis leo ut habitant eget in nam turpis vitae, quam taciti nullam aliquet rhoncus quisque dictumst fusce mattis, vel dui maecenas enim morbi sociosqu himenaeos erat. Fusce penatibus dictum pellentesque odio sagittis lobortis fermentum hendrerit mattis placerat, vel sociis facilisi tortor quisque tempus nisi mus primis, iaculis nisl ac egestas leo pulvinar ante quam vulputate.
-
-              Augue pretium sodales vestibulum lectus interdum scelerisque, orci convallis curabitur feugiat phasellus, curae imperdiet mollis volutpat aliquet. Placerat augue tempus integer dui ante mus vitae, litora urna interdum nulla elementum massa, est sociis porttitor volutpat laoreet viverra. Orci diam conubia nisl quisque eu ornare nulla, faucibus pulvinar congue rhoncus purus dapibus, turpis justo aliquam nullam eleifend vulputate.
-              """
-        }
+        print("Response", response)
+        return response.content
     }
 }
