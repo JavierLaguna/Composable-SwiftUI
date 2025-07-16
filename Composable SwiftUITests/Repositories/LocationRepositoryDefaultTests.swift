36969b7936e3c0d2a3963a337ca16c7dec77fdaa
Javier Laguna <javier.laguna@innocv.com>
2025-07-16 15:35:03 +0200

    Mark LocationRemoteDatasource as Mockable and use on tests

--- Composable SwiftUITests/Repositories/LocationRepositoryDefaultTests.swift
+++ Composable SwiftUITests/Repositories/LocationRepositoryDefaultTests.swift
@@ -1,6 +1,6 @@
 import Testing
 import ComposableArchitecture
-
+import Mockable
 @testable import Composable_SwiftUI
 
 @Suite(
@@ -11,21 +11,45 @@ struct LocationRepositoryDefaultTests {
 
     @Test
     func getCharacterIdsFromLocationSuccess() async throws {
-        let datasource = LocationRemoteDatasourceMock()
-        let repository = LocationRepositoryDefault(service: datasource)
+        let mockDatasource = MockLocationRemoteDatasource()
+        let repository = LocationRepositoryDefault(service: mockDatasource)
+        let mockResponse = LocationResponse(
+            id: 1,
+            name: "Earth",
+            type: "Planet",
+            dimension: "dim",
+            residents: ["url/1", "url/2"]
+        )
+
+        given(mockDatasource)
+            .getLocation(locationId: .any)
+            .willReturn(mockResponse)
 
         let result = try await repository.getCharacterIdsFromLocation(locationId: 1)
 
         #expect(result == [1, 2])
+
+        verify(mockDatasource)
+            .getLocation(locationId: .value(1))
+            .called(.once)
     }
 
     @Test
     func getCharacterIdsFromLocationFail() async throws {
-        let datasource = LocationRemoteDatasourceMock(success: false)
-        let repository = LocationRepositoryDefault(service: datasource)
+        let mockDatasource = MockLocationRemoteDatasource()
+        let mockError: RepositoryError = .invalidUrl
+        let repository = LocationRepositoryDefault(service: mockDatasource)
+
+        given(mockDatasource)
+            .getLocation(locationId: .any)
+            .willThrow(mockError)
 
         try await #require(throws: RepositoryError.invalidUrl) {
             try await repository.getCharacterIdsFromLocation(locationId: 1)
         }
+
+        verify(mockDatasource)
+            .getLocation(locationId: .value(1))
+            .called(.once)
     }
 }
