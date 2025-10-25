5faf99e9959cd3be5be9be370e099cf381dfc3c7
Javier Laguna <javier.laguna@innocv.com>
2025-10-25 13:45:18 +0200

    Create fallback for interactor

--- Composable SwiftUI/Business/Interactors/Character/GetCharacterDescriptionInteractor.swift
+++ Composable SwiftUI/Business/Interactors/Character/GetCharacterDescriptionInteractor.swift
@@ -10,11 +10,16 @@ protocol GetCharacterDescriptionInteractor: Sendable {
 struct GetCharacterDescriptionInteractorFactory {
 
     static func build() -> any GetCharacterDescriptionInteractor {
-        GetCharacterDescriptionInteractorDefault()
+        if #available(iOS 26.0, *) {
+            GetCharacterDescriptionInteractorByAppleIntelligence()
+        } else {
+            GetCharacterDescriptionInteractorLoremIpsum()
+        }
     }
 }
 
-struct GetCharacterDescriptionInteractorDefault: GetCharacterDescriptionInteractor, ManagedErrorInteractor {
+@available(iOS 26.0, *)
+struct GetCharacterDescriptionInteractorByAppleIntelligence: GetCharacterDescriptionInteractor, ManagedErrorInteractor {
 
     private let session = LanguageModelSession()
 
@@ -30,5 +35,23 @@ struct GetCharacterDescriptionInteractorDefault: GetCharacterDescriptionInteract
         }
 
         return ""
+    }
+}
+
+struct GetCharacterDescriptionInteractorLoremIpsum: GetCharacterDescriptionInteractor, ManagedErrorInteractor {
+
+    func execute(character: Character) async throws -> String {
+        if Bool.random() {
+           """
+              Lorem ipsum dolor sit amet consectetur adipiscing, elit cubilia maecenas inceptos rutrum hac, sed faucibus interdum commodo curabitur. Gravida felis leo ut habitant eget in nam turpis vitae, quam taciti nullam aliquet rhoncus quisque dictumst fusce mattis, vel dui maecenas enim morbi sociosqu himenaeos erat. Fusce penatibus dictum pellentesque odio sagittis lobortis fermentum hendrerit mattis placerat, vel sociis facilisi tortor quisque tempus nisi mus primis, iaculis nisl ac egestas leo pulvinar ante quam vulputate.
+              """
+
+        } else {
+           """
+              Lorem ipsum dolor sit amet consectetur adipiscing, elit cubilia maecenas inceptos rutrum hac, sed faucibus interdum commodo curabitur. Gravida felis leo ut habitant eget in nam turpis vitae, quam taciti nullam aliquet rhoncus quisque dictumst fusce mattis, vel dui maecenas enim morbi sociosqu himenaeos erat. Fusce penatibus dictum pellentesque odio sagittis lobortis fermentum hendrerit mattis placerat, vel sociis facilisi tortor quisque tempus nisi mus primis, iaculis nisl ac egestas leo pulvinar ante quam vulputate.
+
+              Augue pretium sodales vestibulum lectus interdum scelerisque, orci convallis curabitur feugiat phasellus, curae imperdiet mollis volutpat aliquet. Placerat augue tempus integer dui ante mus vitae, litora urna interdum nulla elementum massa, est sociis porttitor volutpat laoreet viverra. Orci diam conubia nisl quisque eu ornare nulla, faucibus pulvinar congue rhoncus purus dapibus, turpis justo aliquam nullam eleifend vulputate.
+              """
+        }
     }
 }
---
+++ Composable SwiftUI/Common/Components/LoadingAppleIntelligence.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingAppleIntelligence.swift
+//  Composable SwiftUI
+//
+//  Created by Javier Laguna on 25/10/25.
+//
+
+import SwiftUI
+
+struct LoadingAppleIntelligence: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingAppleIntelligence()
+}
