5461191377490fd26ee5da797e9e66e04d01c7ea
Javier Laguna <javier.laguna@innocv.com>
2026-03-24 18:37:49 +0100

    WIP

--- Composable SwiftUI/Common/Components/LoadingAppleIntelligence.swift
+++ Composable SwiftUI/Common/Components/LoadingAppleIntelligence.swift
@@ -2,15 +2,28 @@ import SwiftUI
 
 struct LoadingAppleIntelligence: View {
 
+    private let text: String?
+
+    init(text: String? = nil) {
+        self.text = text
+    }
+
     var body: some View {
         VStack(spacing: Theme.Space.m) {
             KeyframeAnimator(initialValue: 0.0, repeating: true) { rotation in
                 Image(systemName: "apple.intelligence")
                     .font(.largeTitle)
                     .rotationEffect(.init(degrees: rotation))
+
             } keyframes: { _ in
                 LinearKeyframe(0, duration: 0)
                 LinearKeyframe(360, duration: 5)
+            }
+
+            if let text {
+                Text(text)
+                    .font(.caption)
+                    .foregroundStyle(.gray)
             }
         }
     }
@@ -18,4 +31,6 @@ struct LoadingAppleIntelligence: View {
 
 #Preview {
     LoadingAppleIntelligence()
+
+    LoadingAppleIntelligence(text: "Generating with AI...")
 }
